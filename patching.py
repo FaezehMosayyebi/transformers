e8a34fdb56c1552ac26be3acacde97dbda97994d
unknown <faezeh.mosayyebi@gmail.com>
2024-07-24 17:51:27 +0330

    projection and positional embedding added

--- patching.py
+++ patching.py
@@ -2,23 +2,30 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-import matplotlib.pyplot as plt
 
-class Patches(nn.Module):
-    def __init__(self, patch_size: int):
-        super(Patches, self).__init__()
+class PatcheEmbedding(nn.Module):
+    def __init__(self, image_size: tuple[int, int, int, int], patch_size: int, projection_dim: int):
+        super(PatcheEmbedding, self).__init__()
+
+        self.batch_size, self.channels, height, width = image_size
         self.patch_size = patch_size
+        self.num_patches = (height // self.patch_size) * (width // self.patch_size)
+
+        self.projection = nn.Linear(self.channels*self.patch_size*self.patch_size, projection_dim)
+        self.projection_embedding = nn.Embedding(self.num_patches, projection_dim)
 
     def forward(self, images: torch.Tensor) -> torch.Tensor:
-        batch_size, channels, height, width = images.shape
-        
-        num_patches_h = height // self.patch_size
-        num_patches_w = width // self.patch_size
         
         patches = images.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
-        patches = patches.contiguous().view(batch_size, channels, -1, self.patch_size, self.patch_size)
+        patches = patches.contiguous().view(self.batch_size, self.channels, -1, self.patch_size, self.patch_size)
         patches = patches.permute(0, 2, 3, 4, 1).contiguous()
-        patches = patches.view(batch_size, num_patches_h * num_patches_w, -1)
+        patches = patches.view(self.batch_size, self.num_patches, -1)
+        patches = self.projection(patches)
+
+        positions = torch.arange(start=0, end = self.num_patches, step=1)[None,:]
+        positional_embedding = self.projection_embedding(positions)
+
+        patches = patches + positional_embedding
    
         return patches
 
@@ -27,16 +34,12 @@ class Patches(nn.Module):
 
 
 if __name__ == "__main__":
+    import matplotlib.pyplot as plt
 
     # test
     image = torchvision.io.read_image('images.jpg')
     image = image[None, :]
-    patching = Patches(16)
+    patching = PatcheEmbedding(16)
     patches = patching(image)
 
-    patchsize = patching.get_config()["patch_size"]
-    for i , patch in enumerate(patches[0]):
-        ax = plt.subplot(image.shape[2]//patchsize, image.shape[3]//patchsize, i+1)
-        plt.imshow(patch.view(patchsize, patchsize, 3).numpy().astype("uint8"))
-        plt.axis("off")
-    plt.show()
+    print(patches.shape)
